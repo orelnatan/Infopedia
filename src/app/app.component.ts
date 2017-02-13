31b4517306ac001174ae8e995778ef47ac03d673
unknown <natenorel@gmail.com>
2017-02-13 15:32:36 +0200

    'update_1'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,48 @@
-import { Component } from '@angular/core';
+
+import { Component }        from '@angular/core';
+import { InfoServices }     from './services/infoServices.service';
+import { Entry }            from './classes/entry';
+import                           './operators/rxjs-operators';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: `./app/templates/myAppComponent.template.html`,
+  styleUrls: ['./app/styles/myAppComponent.style.css'],
+
+  providers: [InfoServices],
 })
-export class AppComponent  { name = 'Angular'; }
+
+
+export class AppClass  { 
+
+  notifications = {
+
+     searchImgUrl:    'http://findicons.com/files/icons/887/web/256/explorer.png',
+     titleText:       'Search for anything...',
+     boxHolderText:   'Insert some free text'
+
+  };
+
+  entrys: Entry[] = [];
+
+
+  constructor(private infoServices: InfoServices){
+
+  }
+
+
+  getUserInput(reference: any){
+
+    this.infoServices.getDataFromServer(reference.value).subscribe((response) => {
+                       
+        this.entrys = response;
+        
+    });
+
+   
+    
+
+  }
+
+
+}
